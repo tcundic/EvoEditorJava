049201221e98e492f8f7e8f6a46c0e3024c3148f
Tcundic <tcundic@gmail.com>
2015-03-05 08:07:06 +0100

    Funkcionalno mijenjanje atribute galerije, povezane vrijednosti slidera i numpickera transparentnosti

--- EvoEditor/src/hr/foi/air/evoEditor/controller/GalleryDataPanelController.java
+++ EvoEditor/src/hr/foi/air/evoEditor/controller/GalleryDataPanelController.java
@@ -1,9 +1,26 @@
 package hr.foi.air.evoEditor.controller;
 
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JSlider;
+import javax.swing.JSpinner;
+import javax.swing.JTable;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.text.TableView.TableRow;
+
+import com.sun.org.apache.xml.internal.utils.IntVector;
+
 import hr.foi.air.evoEditor.gui.GalleryDataPanel;
 import hr.foi.air.evoEditor.model.interfaces.IGallery;
 
-public class GalleryDataPanelController {
+public class GalleryDataPanelController implements ChangeListener, ActionListener {
 	
 	private IGallery gallery;
 	private GalleryDataPanel gui;
@@ -20,6 +37,51 @@ public class GalleryDataPanelController {
 		for (String attributeName : gallery.getGalleryAttributeSet()) {
 			if (!gui.getUnchangeableElements().contains(attributeName))
 				gui.addAttributes(attributeName, gallery.getGalleryAttribute(attributeName));
+		}
+		
+		gui.setGalleryTransparency(gallery.getGalleryAttribute("transparency"));
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		System.out.println(e.getSource().getClass());
+		
+		if (e.getSource() instanceof JSpinner) {
+			gui.setSlider(((Number)((JSpinner)e.getSource()).getValue()).intValue());
+		} else if(e.getSource() instanceof JSlider) {
+			gui.setSpinner((int)((JSlider)e.getSource()).getValue());
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (((JButton)e.getSource()).getName().equals("saveGalleryData")) {
+			Component[] components = gui.getComponents();
+			
+			for (Component component : components) {
+				for (String attributeName : gui.getUnchangeableElements()) {
+					if (component.getName() != null) {
+						if (component.getName().equals(attributeName)) {
+							String value = "";
+							if (component instanceof JSpinner) {
+								value = String.valueOf(((Number)((JSpinner)component).getValue()).intValue());
+							} else if (component instanceof JCheckBox) {
+								value = String.valueOf(((JCheckBox)component).isSelected());
+							}
+							gallery.setGalleryAttribute(component.getName(), value);
+						}
+					}
+				}
+			}
+			
+			for (int row = 0; row < gui.getTblGalleryAttributes().getRowCount(); row++) {
+				gallery.setGalleryAttribute((String) gui.getTblGalleryAttributes().getValueAt(row, 0), (String) gui.getTblGalleryAttributes().getValueAt(row, 1));
+			}
+			
+		}
+		
+		for (String attribute : gallery.getGalleryAttributeSet()) {
+			System.out.println(attribute + ": " + gallery.getGalleryAttribute(attribute));
 		}
 	}
 }
--- EvoEditor/src/hr/foi/air/evoEditor/gui/GalleryDataPanel.java
+++ EvoEditor/src/hr/foi/air/evoEditor/gui/GalleryDataPanel.java
@@ -16,12 +16,22 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JSlider;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 import org.eclipse.wb.swing.FocusTraversalOnArray;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GalleryDataPanel extends JPanel {
 
@@ -30,11 +40,10 @@ public class GalleryDataPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = -8738314693024227945L;
 	
-	JTextField textFieldTransparencyValue;
+	JSpinner numberPickerTransparencyValue;
 	JTable tblGalleryAttributes;
 	JSlider transparencySlider;
 	ArrayList<String> unchangeableElements;
-	
 	GalleryDataPanelController controller;
 	
 	
@@ -49,7 +58,7 @@ public class GalleryDataPanel extends JPanel {
 	private void initialize() {
 		unchangeableElements = new ArrayList<String>();
 		super.setMinimumSize(new Dimension(350, 100));
-		super.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
 		JLabel lblGallery = new JLabel("Gallery");
 		super.add(lblGallery);
@@ -58,15 +67,13 @@ public class GalleryDataPanel extends JPanel {
 		super.add(separator);
 		
 		JPanel GalleryAttributesPanel = new JPanel();
+		GalleryAttributesPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		GalleryAttributesPanel.setAlignmentY(Component.TOP_ALIGNMENT);
 		super.add(GalleryAttributesPanel);
-		GalleryAttributesPanel.setLayout(new BorderLayout(0, 0));
 		
 		DefaultTableModel model = new DefaultTableModel();
-		tblGalleryAttributes = new JTable(model);
 		model.addColumn("Attribute name");
 		model.addColumn("Attribute value");
-		
-		GalleryAttributesPanel.add(tblGalleryAttributes, BorderLayout.CENTER);
 		
 		JSeparator separator_1 = new JSeparator();
 		super.add(separator_1);
@@ -76,32 +83,58 @@ public class GalleryDataPanel extends JPanel {
 		
 		unchangeableElements.add("transparency");
 		
-		textFieldTransparencyValue = new JTextField();
-		super.add(textFieldTransparencyValue);
-		textFieldTransparencyValue.setColumns(3);
-		textFieldTransparencyValue.setMaximumSize(new Dimension(40, 20));
+		SpinnerNumberModel spinModel = new SpinnerNumberModel(0.0, 0.0, 256.0, 1.0);
+		numberPickerTransparencyValue = new JSpinner(spinModel);
+		numberPickerTransparencyValue.addChangeListener(controller);
+		numberPickerTransparencyValue.setName("transparency");
+		super.add(numberPickerTransparencyValue);
+		numberPickerTransparencyValue.setMaximumSize(new Dimension(40, 20));
 		
 		transparencySlider = new JSlider();
 		transparencySlider.setMinorTickSpacing(1);
 		transparencySlider.setValue(128);
 		transparencySlider.setMaximum(256);
+		transparencySlider.addChangeListener(controller);
 		super.add(transparencySlider);
 		
 		JSeparator separator_2 = new JSeparator();
 		super.add(separator_2);
 		
 		JCheckBox chckbxGalleryRepeatOption = new JCheckBox("Repeat");
+		chckbxGalleryRepeatOption.setName("repeat");
 		super.add(chckbxGalleryRepeatOption);
 		
 		unchangeableElements.add("repeat");
 		
 		JCheckBox chckbxShowIndicatorOption = new JCheckBox("Show indicator");
+		chckbxShowIndicatorOption.setName("showIndicator");
 		super.add(chckbxShowIndicatorOption);
 		
 		unchangeableElements.add("showIndicator");
 		
 		JButton btnSaveGalleryData = new JButton("Save gallery data");
+		btnSaveGalleryData.setName("saveGalleryData");
+		btnSaveGalleryData.addActionListener(controller);
 		super.add(btnSaveGalleryData);
+		GridBagLayout gbl_GalleryAttributesPanel = new GridBagLayout();
+		gbl_GalleryAttributesPanel.columnWidths = new int[]{1, 0, 0};
+		gbl_GalleryAttributesPanel.rowHeights = new int[]{1, 0, 0};
+		gbl_GalleryAttributesPanel.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		gbl_GalleryAttributesPanel.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		GalleryAttributesPanel.setLayout(gbl_GalleryAttributesPanel);
+		
+		JPanel AttributesPanel = new JPanel();
+		GridBagConstraints gbc_AttributesPanel = new GridBagConstraints();
+		gbc_AttributesPanel.fill = GridBagConstraints.BOTH;
+		gbc_AttributesPanel.gridx = 0;
+		gbc_AttributesPanel.gridy = 0;
+		GalleryAttributesPanel.add(AttributesPanel, gbc_AttributesPanel);
+		AttributesPanel.setLayout(new GridLayout(0, 1, 0, 0));
+		tblGalleryAttributes = new JTable(model);
+		tblGalleryAttributes.setName("galleryAttributes");
+		AttributesPanel.add(tblGalleryAttributes);
+		tblGalleryAttributes.setAlignmentY(Component.TOP_ALIGNMENT);
+		tblGalleryAttributes.setAlignmentX(Component.LEFT_ALIGNMENT);
 		super.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{lblGallery, GalleryAttributesPanel, tblGalleryAttributes, lblTransparency}));
 		
 	}
@@ -115,4 +148,22 @@ public class GalleryDataPanel extends JPanel {
 		return unchangeableElements;
 	}
 
+	public void setGalleryTransparency(String galleryTransparency) {
+		int transparency = (galleryTransparency == "") ? 60 : Integer.parseInt(galleryTransparency);
+		
+		numberPickerTransparencyValue.setValue(transparency);
+		transparencySlider.setValue(transparency);
+	}
+	
+	public void setSpinner(int transparency) {
+		numberPickerTransparencyValue.setValue(transparency);
+	}
+	
+	public void setSlider(int transparency) {
+		transparencySlider.setValue(transparency);
+	}
+
+	public JTable getTblGalleryAttributes() {
+		return tblGalleryAttributes;
+	}
 }

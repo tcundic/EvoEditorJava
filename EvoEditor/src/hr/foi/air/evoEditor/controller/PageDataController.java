00d9e29c146ba68022b3cbd53fee7ed641ad10b7
Tadija <btadija@gmail.com>
2015-03-05 07:32:13 +0100

    PageDataPanel sad prikazuje dvije tablice - za page attribute i za page resource. ComboBox je normalne veličine.

--- EvoEditor/src/hr/foi/air/evoEditor/controller/PageDataController.java
+++ EvoEditor/src/hr/foi/air/evoEditor/controller/PageDataController.java
@@ -2,11 +2,20 @@ package hr.foi.air.evoEditor.controller;
 
 import hr.foi.air.evoEditor.gui.PageDataPanel;
 import hr.foi.air.evoEditor.model.interfaces.IGallery;
+import hr.foi.air.evoEditor.model.interfaces.IPage;
 
-public class PageDataController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+
+public class PageDataController implements TreeSelectionListener, ActionListener{
 	
 	private IGallery gallery;
 	private PageDataPanel gui;
+	
+	private IPage page;
 
 	public PageDataController(IGallery gallery) {
 		this.gallery = gallery;
@@ -14,6 +23,18 @@ public class PageDataController {
 	
 	public void setGui(PageDataPanel gui){
 		this.gui = gui;
+	}
+
+	@Override
+	public void valueChanged(TreeSelectionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
